433a887da748c8081fef4d0e3a8e82d5296428dc
riky nurdiana <riky.nurdiana@tiket.com>
2019-11-09 11:46:00 +0700

    Updates

---
+++ precache-manifest.3b009a27cfbbb9fcc8cb06430d51b286.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5f2abdd7faa43ab84e5e9ed0410f05",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "c070d7d53e767f886577",
+    "url": "/react-todo-list/static/css/main.a97e45d5.chunk.css"
+  },
+  {
+    "revision": "0b6739618ba82a88c665",
+    "url": "/react-todo-list/static/js/2.b8e29553.chunk.js"
+  },
+  {
+    "revision": "c070d7d53e767f886577",
+    "url": "/react-todo-list/static/js/main.9a0fcf9a.chunk.js"
+  },
+  {
+    "revision": "fe583da195f7527851f8",
+    "url": "/react-todo-list/static/js/runtime-main.aa700920.js"
+  }
+]);
